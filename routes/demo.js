0856c6fb6f6559bcee5374bbd30eb2d86beb46ae
邹亚 <zouya@corp.netease.com>
2016-01-21 10:39:50 +0800

    页面的添加

--- routes/demo.js
+++ routes/demo.js
@@ -1,6 +1,26 @@
 /* GET demo page. */
 exports.page = function(req, res) {
-	res.render('demo', {
-		title: 'demo'
+	// res.render('demo', {
+	// 	title: 'demo',
+	// 	uName: ''
+	// });
+	
+	
+	// res.render('demo2', {
+	// 	title: 'demo2',
+	// 	uName: ''
+	// });
+
+	
+	// res.render('demo3', {
+	// 	title: 'demo3',
+	// 	uName: ''
+	// });
+	
+	res.render('demo4', {
+		title: 'demo4',
+		uName: ''
 	});
+	
+
 };
